7f49eb4d66f15f12ce03810fb41c0944f2371cfa
MuhammadAdil006 <83723564+MuhammadAdil006@users.noreply.github.com>
2022-06-27 09:43:50 +0500

    asynchoronous using time interval

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 console.log("1");
 console.log("2");
+setTimeout(()=>{
+    console.log("call back function fired");
+},2000);//it will take time but fire after 2 seconds
 console.log("3");
 console.log("4");
